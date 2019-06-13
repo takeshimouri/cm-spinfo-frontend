2b9169842f0b1ca0650ac52515d26ef69df6c39c
mouri <tt.mouri@gmail.com>
2019-06-13 11:26:34 +0900

    InitialCommit

---
+++ src/components/Book.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { removeBook } from '../actions/books';
+
+const Book = ({ id, title, description, author, published, dispatch }) => (
+    <div>
+            <h4>{title} ({published})</h4>
+        <Link to={`/book/${id}`}>
+            Move
+        </Link>
+        <p>Author: {author}</p>
+        {description && <p>{description}</p>}
+        <button onClick={() => {
+            dispatch(removeBook({ id }));
+        }}>Remove</button>
+    </div>
+);
+
+export default connect()(Book);
